5347c2e7f235fc027d00b2f5375df45a6e201cc8
ltt230205 <letritung2302@gmail.com>
2024-12-04 22:08:09 +0700

    first commit

---
+++ HW1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lambaitapoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lê Trí Tùng\t12423037\t12423TN\thttps://github.com/ltt230205\tletritung2302@gmail.com");
+        }
+    }
+}
